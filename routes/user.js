ee4743c9d438640fe190236a5ae0766fbb170106
ashujarodia <112541439+ashujarodia@users.noreply.github.com>
2024-02-04 16:30:10 +0530

    commited

---
+++ routes/user.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { getMyProfile, login, logout, registerUser } from '../controllers/user.js';
+import { isAuthenticated } from '../middlewares/auth.js';
+
+const router = express.Router();
+
+router.post('/new', registerUser);
+router.post('/login', login);
+router.get('/me', isAuthenticated, getMyProfile);
+router.get('/logout', logout);
+
+export default router;
